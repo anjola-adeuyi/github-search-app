26e718b4487919ab381d363b5101f9362879cd1e
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-01-14 15:16:33 +0100

    UserResult Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
+import UserResults from "../components/users/UserResults"
+
 const Home = () => {
+
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-      {process.env.REACT_APP_GITHUB_TOKEN}
-    </div>
+    <>
+      {/* SEARCH COMPONENTS */}
+      <UserResults />
+    </>
   )
 }
 
